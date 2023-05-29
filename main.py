35c774fb0eb445f571a173fc57abe58636dcee9c
Vova <vova.minin2010@gmail.com>
2023-05-29 19:51:56 +0300

    Исправил функцию

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import library
 
 def main():
     schedule.every().day.at('22:00').do(library.sleep_time)
-    schedule.every().monday.at('07:00').do(library.sleep_time)
-    schedule.every().tuesday.at('07:00').do(library.sleep_time)
-    schedule.every().wednesday.at('07:00').do(library.sleep_time)
-    schedule.every().thursday.at('07:00').do(library.sleep_time)
-    schedule.every().friday.at('07:00').do(library.sleep_time)
+    schedule.every().monday.at('07:00').do(library.greeting)
+    schedule.every().tuesday.at('07:00').do(library.greeting)
+    schedule.every().wednesday.at('07:00').do(library.greeting)
+    schedule.every().thursday.at('07:00').do(library.greeting)
+    schedule.every().friday.at('07:00').do(library.greeting)
     while True:
         schedule.run_pending()
 
